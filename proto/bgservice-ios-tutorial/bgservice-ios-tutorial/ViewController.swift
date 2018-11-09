f5314407c50a1286fba02a5433b25cfea8f16f07
Sushant Sahu <sushant87@gmail.com>
2018-11-09 21:12:11 +0530

    initialised restapi-ios-tutorial, bgservice-ios-tutorial

---
+++ proto/bgservice-ios-tutorial/bgservice-ios-tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bgservice-ios-tutorial
+//
+//  Created by Sushant Sahu on 09/11/18.
+//  Copyright © 2018 Sushant Sahu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
